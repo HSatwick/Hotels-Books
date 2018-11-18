db04d1f05875357fa1dc448b2f34b6aab98b28bd
Himanshu Satwick <himanshusatwick60@gmail.com>
2018-11-18 17:12:18 -0500

    adding materials and altering navigation component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from "@angular/core";
-import {MatButtonModule, MatCheckboxModule, MatBadgeModule} from '@angular/material';
+import {
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatBadgeModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatMenuModule,
+    MatListModule
+} from '@angular/material';
 
 @NgModule({
     imports: [
         MatButtonModule,
         MatCheckboxModule,
-        MatBadgeModule
+        MatBadgeModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatMenuModule,
+        MatListModule,
+        MatTabsModule
     ],
     exports: [
         MatButtonModule,
         MatCheckboxModule,
-        MatBadgeModule
+        MatBadgeModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatMenuModule,
+        MatListModule,
+        MatTabsModule
     ]
 })
 
--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() SideNavigationOpenToggle = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onToggleSidenav(){
+
+    this.SideNavigationOpenToggle.emit();
+
   }
 
 }
